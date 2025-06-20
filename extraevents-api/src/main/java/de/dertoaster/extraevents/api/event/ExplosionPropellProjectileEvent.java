8c524a167e8e75833557e2b20787459b28599904
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-06-20 14:17:31 +0200

    change exploder to entity; add event for propelling projectiles

--- extraevents-api/src/main/java/de/dertoaster/extraevents/api/event/ExplosionPropellProjectileEvent.java
+++ extraevents-api/src/main/java/de/dertoaster/extraevents/api/event/ExplosionPropellProjectileEvent.java
@@ -1,7 +1,7 @@
 package de.dertoaster.extraevents.api.event;
 
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.TNTPrimed;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.entity.EntityEvent;
@@ -9,7 +9,7 @@ import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class ExplosionPropellTNTEvent extends EntityEvent implements Cancellable {
+public class ExplosionPropellProjectileEvent extends EntityEvent implements Cancellable {
 
     private static final HandlerList handlers = new HandlerList();
 
@@ -18,19 +18,19 @@ public class ExplosionPropellTNTEvent extends EntityEvent implements Cancellable
     private boolean cancelled = false;
     private final Vector pushDirection;
 
-    public ExplosionPropellTNTEvent(@NotNull TNTPrimed theEntity, @Nullable Entity exploder, @NotNull Vector pushDirection) {
+    public ExplosionPropellProjectileEvent(@NotNull Projectile theEntity, @Nullable Entity exploder, @NotNull Vector pushDirection) {
         super(theEntity);
         this.exploder = exploder;
         this.pushDirection = pushDirection;
     }
 
     @Nullable
-    public Entity getExploder() {
+    public Entity getPusher() {
         return this.exploder;
     }
 
-    public TNTPrimed getProjectile() {
-        return (TNTPrimed) this.entity;
+    public Projectile getProjectile() {
+        return (Projectile) this.entity;
     }
 
     public HandlerList getHandlers() {
@@ -54,4 +54,5 @@ public class ExplosionPropellTNTEvent extends EntityEvent implements Cancellable
     public void setCancelled(boolean b) {
         this.cancelled = b;
     }
+
 }
