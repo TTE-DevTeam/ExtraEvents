f2d0538a0f8b141b8528ed1475a0dcfd731095bf
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-06-20 14:17:52 +0200

    fire the event

--- ServerMixin/src/main/java/de/dertoaster/extraevents/mixin/MixinServerExplosion.java
+++ ServerMixin/src/main/java/de/dertoaster/extraevents/mixin/MixinServerExplosion.java
@@ -2,11 +2,13 @@ package de.dertoaster.extraevents.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import de.dertoaster.extraevents.api.event.ExplosionPropellProjectileEvent;
 import de.dertoaster.extraevents.api.event.ExplosionPropellTNTEvent;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.PrimedTnt;
 import net.minecraft.world.level.ServerExplosion;
 import net.minecraft.world.phys.Vec3;
+import org.bukkit.entity.Projectile;
 import org.bukkit.entity.TNTPrimed;
 import org.bukkit.util.Vector;
 import org.spongepowered.asm.mixin.Mixin;
@@ -33,18 +35,27 @@ public abstract class MixinServerExplosion {
     @Local(name = "entity") Entity entity,
     @Local(name = "vec3") LocalRef<Vec3> vec3
   ) {
-    if (!(entity instanceof PrimedTnt)) {
+    if (!(entity instanceof PrimedTnt || entity instanceof Projectile)) {
       return;
     }
     Entity exploder = ((ServerExplosion)(Object)this).getDirectSourceEntity();
-    TNTPrimed exploderBukkit = null;
-    if (exploder != null && (exploder.getBukkitEntity() instanceof TNTPrimed)) {
-      exploderBukkit = (TNTPrimed) exploder.getBukkitEntity();
+    if (exploder.isRemoved()) {
+      exploder = null;
     }
     Vec3 vec3Raw = vec3.get();
-    ExplosionPropellTNTEvent propellTNTEvent = new ExplosionPropellTNTEvent((TNTPrimed) entity.getBukkitEntity(), exploderBukkit, new Vector(vec3Raw.x, vec3Raw.y, vec3Raw.z));
-    if (!propellTNTEvent.callEvent()) {
-      vec3.set(Vec3.ZERO);
+
+    if (entity instanceof PrimedTnt) {
+      ExplosionPropellTNTEvent propellTNTEvent = new ExplosionPropellTNTEvent((TNTPrimed) entity.getBukkitEntity(), exploder.getBukkitEntity(), new Vector(vec3Raw.x, vec3Raw.y, vec3Raw.z));
+      if (!propellTNTEvent.callEvent()) {
+        vec3.set(Vec3.ZERO);
+      }
+    }
+
+    if (entity instanceof Projectile) {
+      ExplosionPropellProjectileEvent propellProjectileEvent = new ExplosionPropellProjectileEvent((Projectile) entity.getBukkitEntity(), exploder.getBukkitEntity(), new Vector(vec3Raw.x, vec3Raw.y, vec3Raw.z));
+      if (!propellProjectileEvent.callEvent()) {
+        vec3.set(Vec3.ZERO);
+      }
     }
   }
 
