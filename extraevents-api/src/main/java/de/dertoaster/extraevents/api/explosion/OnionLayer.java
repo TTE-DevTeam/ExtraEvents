affb30fd0fa959acd44c9552ee38806078b6e532
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2026-02-01 19:44:37 +0100

    add bitmap to onionlayer; maybe remove later if unnecessary

--- extraevents-api/src/main/java/de/dertoaster/extraevents/api/explosion/OnionLayer.java
+++ extraevents-api/src/main/java/de/dertoaster/extraevents/api/explosion/OnionLayer.java
@@ -1,6 +1,25 @@
 package de.dertoaster.extraevents.api.explosion;
 
+import de.dertoaster.extraevents.api.util.Byte3DBitmap;
+
 import java.util.ArrayList;
 
 public class OnionLayer extends ArrayList<CoveringVector> {
+
+    private final Byte3DBitmap positionMap;
+
+    public OnionLayer(final int radius) {
+        int radToUse = radius + 1;
+        this.positionMap = new Byte3DBitmap(-radToUse, -radToUse, -radToUse, radToUse, radToUse, radToUse);
+    }
+
+    @Override
+    public boolean add(CoveringVector coveringVector) {
+        if (super.add(coveringVector)) {
+            this.positionMap.set(coveringVector);
+            return true;
+        }
+        return false;
+    }
+
 }
